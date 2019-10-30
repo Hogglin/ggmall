21b8f5305af472bb7710dcecf6e00c1819cd7ba4
lemonKing <849611680@qq.com>
2019-10-30 18:06:28 +0800

    UserController

--- mall-user/src/main/java/com/lwj/ggmall/controller/UserController.java
+++ mall-user/src/main/java/com/lwj/ggmall/controller/UserController.java
@@ -3,8 +3,9 @@ package com.lwj.ggmall.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
     @RequestMapping("index")
